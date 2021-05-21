8e3a1d747cd2d57c33462d086e0513fd4fa4d1eb
Ken Xu <kxu@its.ricoh.co.nz>
2021-05-21 16:17:44 +1200

    updated the csv file path

--- IaC-ARMDeployTest/keyvault.ps1
+++ IaC-ARMDeployTest/keyvault.ps1
@@ -9,7 +9,7 @@ $KeyVault = "kv-kxu-01"
 #HINT: KeyVault Name
 #}
 #Loading Default Secrets
-$secrets = Import-Csv 'keyvault.csv'
+$secrets = Import-Csv '/home/vsts/work/1/a/keyvault.csv'
 #HINT: Drop Location
 function New-RandomPassword() {
     param (
