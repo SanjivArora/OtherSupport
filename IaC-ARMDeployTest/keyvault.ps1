c6b4d5f6ff3ed98e922f233c5306b8ea17ddce63
Ken Xu <kxu@ricoh.co.nz>
2021-05-21 21:34:40 +1200

    test yml

--- IaC-ARMDeployTest/keyvault.ps1
+++ IaC-ARMDeployTest/keyvault.ps1
@@ -9,7 +9,7 @@ $KeyVault = "kv-kxu-01"
 #HINT: KeyVault Name
 #}
 #Loading Default Secrets
-$secrets = Import-Csv '_DevTestKXU - identity/Drop/keyvault.csv'
+$secrets = Import-Csv 'keyvault.csv'
 #HINT: Drop Location
 function New-RandomPassword() {
     param (
