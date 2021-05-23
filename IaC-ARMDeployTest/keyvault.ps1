afcc1672fcdbc3ef48ddd433ac0377adc1be4bd5
Ken Xu <kxu@its.ricoh.co.nz>
2021-05-24 10:55:38 +1200

    updated ymal and PS1 files

--- IaC-ARMDeployTest/keyvault.ps1
+++ IaC-ARMDeployTest/keyvault.ps1
@@ -4,50 +4,21 @@ $KeyVault = "kv-kxu-01"
 
 $secrets = Import-Csv 'keyvault.csv'
 
-Function New-RandomPassword {
- 
-    [CmdletBinding()]
-    param(
-        [Parameter(
-            Position = 0,
-            Mandatory = $false
-        )]
-        [ValidateRange(5,79)]
-        [int]    $Length = 16,
- 
-        [switch] $ExcludeSpecialCharacters
- 
+function New-RandomPassword() {
+    param (
+        [int]$size
     )
- 
-    BEGIN {
-        $SpecialCharacters = @((33,35) + (36..38) + (42..44) + (60..64) + (91..94))
-    }
- 
-    PROCESS {
-        try {
-            if (-not $ExcludeSpecialCharacters) {
-                    $Password = -join ((48..57) + (65..90) + (97..122) + $SpecialCharacters | Get-Random -Count $Length | foreach {[char]$_})
-                } else {
-                    $Password = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count $Length | foreach {[char]$_})
-            }
- 
-        } catch {
-            Write-Error $_.Exception.Message
-        }
- 
-    }
- 
-    END {
-        Write-Output $Password
-    }
- 
+    $minLength = 8 ## characters
+    $maxLength = $size ## characters
+    $lengh = Get-Random -Minimum $minLength -Maximum $maxLength
+    $nonAlphaChars = $minLength
+    $password = [system.web.security.membership]::GeneratePassword($lengh, $nonAlphaChars)
+    return $password
 }
-
-# Generate Random Passwords by calling the function above
 
 foreach ($secret in $secrets) {
     If ($secret.secret -eq 'random') {
-        $tmpSecret = ConvertTo-SecureString (New-RandomPassword) -AsPlainText -Force
+        $tmpSecret = ConvertTo-SecureString (New-RandomPassword -size 14) -AsPlainText -Force
     }
     Else {
         $tmpSecret = ConvertTo-SecureString $secret.secret -AsPlainText -Force
