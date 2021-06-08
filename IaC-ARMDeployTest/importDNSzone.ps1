180b172e7609ccd0c019a508fb79e3b34feeb1f5
Yan Xu <kenx@outlook.co.nz>
2021-06-08 21:51:30 +1200

    updated PS1

--- IaC-ARMDeployTest/importDNSzone.ps1
+++ IaC-ARMDeployTest/importDNSzone.ps1
@@ -1,1 +1,1 @@
-az network private-dns zone import -g IaC-ARMDeployTest-RG -n getmodern.co.nz -f '/home/vsts/work/1/a/getmodern.co.nz.dns'
+az network private-dns zone import -g IaC-ARMDeployTest-RG -n getmodern.co.nz -f getmodern.co.nz.dns
